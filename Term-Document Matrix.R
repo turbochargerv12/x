55e092f130452346edfac4b999af74fab6ba47f0
Roshan Reji <roshanreji2023@it.ajce.in>
2023-02-06 23:56:13 +0530

    Update Term-Document Matrix.R

--- Term-Document Matrix.R
+++ Term-Document Matrix.R
@@ -4,23 +4,8 @@ install.packages("tm")
 # Load tm package
 library(tm)
 
-# Load crude data
 data("crude")
-
-# Create Term Document Matrix
-tdm <- TermDocumentMatrix(crude, control = list(removePunctuation = TRUE, stopwords = TRUE))
-
-# Inspect first 20 terms of documents 100 through 150
-inspect(tdm[100:150, 1:20])
-
-# Inspect terms "price", "prices", and "texas" in documents "127", "144", "191", and "194"
-inspect(tdm[c("price", "prices", "texas"), c("127", "144", "191", "194")])
-
-# Inspect terms "prices" and "oil" in document "191"
-inspect(tdm[c("prices","oil"), c("191")])
-
-# Create Document Term Matrix
-dtm <- DocumentTermMatrix(crude, control = list(removePunctuation = TRUE, stopwords = TRUE))
-
-# Inspect first 20 documents of terms 100 through 150
-inspect(dtm[1:20, 100:150])
+ tdm=TermDocumentMatrix(crude)
+ inspect(tdm[100:150,1:20])
+ dtm=DocumentTermMatrix(crude)
+ inspect(dtm[1:20,100:150])
