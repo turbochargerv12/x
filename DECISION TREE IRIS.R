9598535188d10704de87ebdb959634a36e3929fd
Roshan Reji <roshanreji2023@it.ajce.in>
2023-02-06 23:53:31 +0530

    Update DECISION TREE IRIS.R

--- DECISION TREE IRIS.R
+++ DECISION TREE IRIS.R
@@ -1,48 +1,19 @@
 library(caret)
+
 library(rpart.plot)
 
-# Load the Iris Dataset
-irisdata <- datasets::iris
+intrain=createDataPartition(y=iris$Species,p=0.7,list = FALSE)
 
-# Check the Attributes and Class Distribution in the Data
-head(iris)
-table(iris$Species)
+training=iris[intrain,]
 
-# Summary of Data
-summary(irisdata)
+testing=iris[-intrain,]
 
-# Structure of Data
-str(irisdata)
+trctrl=trainControl(method = "repeatedcv",number = 10,repeats =3)
 
-# Split Data into Training and Testing Sets
-set.seed(3033)
-intrain <- createDataPartition(y = irisdata$Species, p = 0.7, list = FALSE)
-training <- irisdata[intrain,]
-testing <- irisdata[-intrain,]
+dtree_fit_info <- train(Species ~., data = training, method = "rpart", parms = list(split = "gini(or information)"), trControl=trctrl)
 
-# Train a Decision Tree Model with Information Gain Split Criteria
-trctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
-set.seed(3333)
-dtree_fit_info <- train(Species ~ ., data = training, method = "rpart",
-                        parms = list(split = "information"), trControl = trctrl,
-                        tuneLength = 10)
+prp(dtree_fit_info$finalModel)
+ 
+test_info=predict(dtree_fit_info,newdata=testing)
 
-# Plot the Trained Model
-prp(dtree_fit_info$finalModel, box.palette = "Reds", tweak = 1.2)
-
-# Predict the Outcome for Testing Data
-test_pred_info <- predict(dtree_fit_info, newdata = testing)
-confusionMatrix(test_pred_info, testing$Species)
-
-# Train a Decision Tree Model with Gini Coefficient Split Criteria
-set.seed(3333)
-dtree_fit_gini <- train(Species ~ ., data = training, method = "rpart",
-                        parms = list(split = "gini"), trControl = trctrl,
-                        tuneLength = 10)
-
-# Plot the Trained Model
-prp(dtree_fit_gini$finalModel, box.palette = "Blues", tweak = 1.2)
-
-# Predict the Outcome for Testing Data
-test_pred_gini <- predict(dtree_fit_gini, newdata = testing)
-confusionMatrix(test_pred_gini, testing$Species)
+confusionMatrix(test_info,testing$Species)
