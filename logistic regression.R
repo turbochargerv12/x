68b3beff44a52ff47186a45183ae187eabdeffd8
Roshan Reji <roshanreji2023@it.ajce.in>
2023-02-06 23:50:51 +0530

    Update logistic regression.R

--- logistic regression.R
+++ logistic regression.R
@@ -1,9 +1,2 @@
-input <- mtcars[,c("am","cyl","hp","wt")]
-
-print(head(input))
-
-input <- mtcars[,c("am","cyl","hp","wt")]
-
-am.data = glm(formula = am ~ cyl + hp + wt, data = input, family = binomial)
-
-print(summary(am.data))
+a=glm(formula = am~cyl+hp+wt,data = mtcars,family = binomial)
+print(summary(a))
